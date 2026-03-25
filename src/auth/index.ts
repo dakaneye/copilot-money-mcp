b3c9228749b8e7b7d202af8b4a94018be9f261ce
Samuel Dacanay <sam.dacanay@chainguard.dev>
2026-03-25 14:01:53 -0700

    chore: remove unused browser auth module

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,3 +1,2 @@
 export { AuthManager, getAuthManager } from './manager.js';
 export { getStoredTokens, storeTokens, clearTokens } from './keychain.js';
-export { performBrowserAuth } from './browser.js';
