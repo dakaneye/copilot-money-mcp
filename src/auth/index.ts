1d214d1ce666aaed289e4daad0d82bddff1f7d60
Samuel Dacanay <sam.dacanay@chainguard.dev>
2026-03-25 14:55:15 -0700

    chore: export new auth modules

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,2 +1,4 @@
 export { AuthManager, getAuthManager } from './manager.js';
 export { getStoredTokens, storeTokens, clearTokens } from './keychain.js';
+export { isPlaywrightAvailable, captureTokenWithPlaywright } from './playwright.js';
+export { captureTokenWithEmailLink } from './email-link.js';
